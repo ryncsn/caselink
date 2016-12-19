6215c7b4af801deb2d62e60e120c4d06f53fc506
ryncsn <ryncsn@gmail.com>
2016-12-19 23:36:40 +0800

    Restruct shareParameters.js

--- caselink/static/javascript/lib/sharedParameters.js
+++ caselink/static/javascript/lib/sharedParameters.js
@@ -1,8 +1,8 @@
 //Helper for sharing some parameters between template and frontend framework
-//Like common used URL, host URL, parameters in settting.py
-//to avoid declearing them twice
-//Need to loaded at very beginning.
 (function (root, factory) {
+  if (typeof root !== 'object'){
+    throw new Error("Not supported enviroment");
+  }
   if (typeof define === 'function' && define.amd) {
     define(['exports'], function (exports) {
       factory((root.sharedParameters = exports));
@@ -12,18 +12,12 @@
   } else {
     factory((root.sharedParameters = {}));
   }
-}(this || window || {}, function (exports) {
-  if(typeof window === 'object'){
-    window._templateParameters = {};
-    exports.set = function(name, value){
-      window._templateParameters[name] = value;
-    };
-    exports.get = function(name){
-      return window._templateParameters[name];
-    };
-    exports = window;
-  }
-  else {
-    throw new Error("template-parameters doesn't support non-browser enviroment.");
-  }
+}(this || window, function (exports) {
+  exports._sharedParameters = exports._sharedParameters || {};
+  exports.set = function(name, value){
+    exports._sharedParameters[name] = value;
+  };
+  exports.get = function(name){
+    return exports._sharedParameters[name];
+  };
 }));

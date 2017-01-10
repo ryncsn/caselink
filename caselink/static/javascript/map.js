9e545b36d9a0bf45738079fa8d79d9bb72b790a5
Kairui Song <kasong@redhat.com>
2017-01-10 11:31:43 +0800

    Refine the linkage graph

--- caselink/static/javascript/map.js
+++ caselink/static/javascript/map.js
@@ -15,15 +15,19 @@ var vm = new Vue({
     $.get("data?type=m2a")
       .done(function(data){
         let manualcases = data.data.filter((a) => {return a.cases.length > 0;}),
-          autocases = manualcases.reduce((a, b) => { return a.concat(b.cases.reduce((x, y) => { x.push({case: y}); return x; }, [])); }, []),
+          autoDict = manualcases.reduce((a, b) => {(b.cases.reduce((x, y) => {a[y]?a[y].count += 1: a[y] = {case: y, count: 1};}, null)); return a;}, {}),
           manualDict = manualcases.reduce(function(a, b){a[b.polarion] = b; return a;}, {}),
-          autoDict = autocases.reduce(function(a, b){a[b.case] = b; return a;}, {});
+          autocases = d3.values(autoDict);
 
         let svg = d3.select("#linkage-map-svg"),
           width = +svg.attr("width"),
           height = +svg.attr("height"),
           g = svg.append("g"),
-          scaleX = scaleLinear().range([0, width]).domain([0, Math.max(manualcases.length, autocases.length)]);
+          maxLength = Math.max(manualcases.length, autocases.length),
+          mRatio = manualcases.length / maxLength,
+          aRatio = autocases.length / maxLength,
+          mScaleX = scaleLinear().range([0, width]).domain([0, manualcases.length]),
+          aScaleX = scaleLinear().range([0, width]).domain([0, maxLength]);
 
         var manualG = g.append("g"),
           manual = manualG.selectAll('circle')
@@ -31,7 +35,7 @@ var vm = new Vue({
           .enter()
           .append("circle")
           .attr("cy",function(d,i){d.y = 100; return d.y;})
-          .attr("cx",function(d,i){d.x = scaleX(i); return d.x;})
+          .attr("cx",function(d,i){d.x = mScaleX(i); return d.x;})
           .attr("fill","blue").attr("stroke","black")
           .attr("r",2);
 
@@ -41,18 +45,36 @@ var vm = new Vue({
           .enter()
           .append("circle")
           .attr("cy",function(d,i){d.y = 700; return d.y;})
-          .attr("cx",function(d,i){d.x = scaleX(i); return d.x;})
+          .attr("cx",function(d,i){d.x = aScaleX(i); return d.x;})
           .attr("fill","pink").attr("stroke","black")
           .attr("r",2);
 
-        var links = manualcases.reduce((a, b) => { return a.concat(b.cases.reduce((x,y) => {x.push([b.polarion, y]); return x;}, [])); }, []);
+        var links = manualcases.reduce((a, b) => { return a.concat(b.cases.reduce((x,y) => {x.push(
+          [b.polarion, y,
+            (autoDict[y].count == 1 && manualDict[b.polarion].cases.length == 1)?'s2s':
+            (autoDict[y].count == 1)?'s2m':
+            (manualDict[b.polarion].cases.length == 1)?'m2s':
+            'm2m']
+        ); return x;}, [])); }, []),
+          color = {
+            'm2m': 'red',
+            's2s': 'black',
+            's2m': 'blue',
+            'm2s': 'green',
+          },
+          opacity = {
+            'm2m': 0.25,
+            's2s': 0.5,
+            's2m': 0.25,
+            'm2s': 0.75,
+          };
         var lineG = g.append("g"),
           line = lineG.selectAll('line')
           .data(links)
           .enter()
           .append("line")
-          .style("stroke", function(d){return 'black';})
-          .style("stroke-opacity", function(d){return 0.03;})
+          .style("stroke", function(d){return color[d[2]];})
+          .style("stroke-opacity", function(d){return opacity[d[2]];})
           .attr("x1",function(d){return manualDict[d[0]].x;})
           .attr("y1",function(d){return manualDict[d[0]].y;})
           .attr("x2",function(d){return autoDict[d[1]].x;})

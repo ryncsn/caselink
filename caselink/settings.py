fa3ec4d9d9499c6218125eebc683805eddc1be87
ryncsn <ryncsn@gmail.com>
2017-01-23 11:23:56 +0100

    Add STATIC_ROOT for collectstatic

--- caselink/settings.py
+++ caselink/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/dist/')
 
 FIXTURE_DIRS = [
     'caselink/fixture/'
